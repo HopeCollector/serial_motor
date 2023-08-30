f7c281c42f3488aab0d3c546e197f5c24eca552f
HopeCollector <cmw4187@126.com>
2023-08-30 14:00:09 +0800

    fix<demo>: 修改 motor 代码，使其适应现在的库

--- demo/motor.cpp
+++ demo/motor.cpp
@@ -5,48 +5,37 @@
 #include <string>
 #include <vector>
 
-#include "serial_motor/types/rmd.hpp"
+#include "serial_motor/motor.hpp"
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   std::string dev("");
   dev = std::string(argv[1]);
-  if (dev.empty()) {
-    std::cerr << "cannot find motor device" << std::endl;
-    exit(-1);
-  };
+  auto m = smotor::create_rmd_motor(dev, 1);
 
-  smotor::rmd m(dev, 1);
-
-  std::vector<double> params;
   std::vector<std::string> args;
-  for (int i = 1; i < argc; i++) {
-    if (argv[i][0] == '-') {
-      while (i < argc) {
-        args.push_back(std::string(argv[i]));
-        i++;
-      }
-    }
+  for (int i = 2; i < argc; i++) {
+    args.push_back(argv[i]);
   }
 
   if (args[0] == "-t") {
-      m.to(std::stod(args[1]), std::stod(args[2]));
+    m->to(std::stod(args[1]), std::stod(args[2]));
   } else if (args[0] == "-m") {
-      m.more(std::stod(args[1]), std::stod(args[2]));
+    m->more(std::stod(args[1]), std::stod(args[2]));
   } else if (args[0] == "-r") {
-      m.rotate(std::stod(args[1]));
+    m->rotate(std::stod(args[1]));
   } else if (args[0] == "-p") {
-    m.pause();
+    m->pause();
   } else if (args[0] == "-g") {
     // auto start = std::chrono::system_clock::now();
-    // for (size_t i = 0; i < 1000; i++)
-    // {
-    //    std::cout << m.getCurrentPose() << std::endl;
+    // for (size_t i = 0; i < 1000; i++) {
+    //   // std::cout << m->cur_pose().value_or(0.0) << std::endl;
+    //   m->cur_pose();
     // }
     // auto end = std::chrono::system_clock::now();
     // auto time = std::chrono::duration<double>(end - start).count();
     // std::cout << "total cost: " << time << std::endl;
     // std::cout << "echo freq: " << 1000 / time << "hz" << std::endl;
-    std::cout << m.cur_pose().value_or(0.0) << std::endl;
+    std::cout << m->cur_pose().value_or(0.0) << std::endl;
   }
 
   return 0;
